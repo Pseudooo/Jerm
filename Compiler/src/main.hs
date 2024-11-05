fb094c5f838df38e34fe10f75295b104aaa43193
Mitchell Wright <mitchell.wright877@gmail.com>
2024-11-05 21:39:08 +0000

    Transpilation for if statements

--- Compiler/src/main.hs
+++ Compiler/src/main.hs
@@ -37,11 +37,21 @@ asWords xs = mapM_ putWord32le (map fromIntegral xs)
 --}
 transpile :: [Statement] -> [Int]
 transpile ss = let locals = assignLocalOffsets ss in
-    localsSize ss : maxStackSize ss : foldr1 (++) (map (transpileStatement locals) ss)
+    localsSize ss : maxStackSize ss : transpileStatements locals ss
+
+transpileStatements :: [StackLocal] -> [Statement] -> [Int]
+transpileStatements locals ss = foldr1 (++) (map (transpileStatement locals) ss)
 
 transpileStatement :: [StackLocal] -> Statement -> [Int]
 transpileStatement stackLocals (VariableInitialisation name expr) = transpileExpression stackLocals expr ++ [2] ++ [getLocalOffset stackLocals name]
-transpileStatement stackLocals (VariableAssignment name expr) = transpileExpression stackLocals expr ++ [2] ++ [getLocalOffset stackLocals name] 
+transpileStatement stackLocals (VariableAssignment name expr) = transpileExpression stackLocals expr ++ [2] ++ [getLocalOffset stackLocals name]
+transpileStatement stackLocals (IfStatement predicate body) = let transpiledBody = transpileStatements stackLocals body in
+    transpileExpression stackLocals predicate ++ [11, length transpiledBody] 
+transpileStatement stackLocals (IfElseStatement predicate ifBody elseBody) = transpiledPredicate ++ [11, length transpiledIfBody + 4] ++ transpiledIfBody ++ [10, length transpiledElseBody + 2] ++ transpiledElseBody
+    where
+        transpiledPredicate = transpileExpression stackLocals predicate
+        transpiledIfBody = transpileStatements stackLocals ifBody
+        transpiledElseBody = transpileStatements stackLocals elseBody
 
 getLocalOffset :: [StackLocal] -> String -> Int
 getLocalOffset [] _ = -1
@@ -56,6 +66,14 @@ assignLocalOffsets statements = assignLocalOffsets' 0 statements
         assignLocalOffsets' _ [] = []
         assignLocalOffsets' i (x:xs) = case x of
             VariableInitialisation name _ -> (name, i) : assignLocalOffsets' (i + 1) xs
+
+            IfStatement _ body -> let nestedAssignments = assignLocalOffsets' i body in
+                nestedAssignments ++ assignLocalOffsets' (length nestedAssignments + i) xs
+
+            IfElseStatement _ ifBody elseBody -> let ifBodyAssignments = assignLocalOffsets' i ifBody in
+                let elseBodyAssignments = assignLocalOffsets' (length ifBodyAssignments + i) elseBody in
+                    ifBodyAssignments ++ elseBodyAssignments ++ assignLocalOffsets' (length ifBodyAssignments + length elseBodyAssignments + i) xs
+
             _ -> assignLocalOffsets' i xs
 
 transpileExpression :: [StackLocal] -> Expression -> [Int]
@@ -78,6 +96,8 @@ operatorOpcode Or = 9
 localsSize :: [Statement] -> Int
 localsSize (x:xs) = case x of
     (VariableInitialisation _ _) -> 1 + localsSize xs
+    (IfStatement _ ifBody) -> localsSize ifBody + localsSize xs
+    (IfElseStatement _ ifBody elseBody) -> localsSize ifBody + localsSize elseBody + localsSize xs
     _ -> localsSize xs
 localsSize [] = 0
 
@@ -87,6 +107,8 @@ maxStackSize xs = foldr max 0 (map statementRequiredStackSize xs)
 statementRequiredStackSize :: Statement -> Int
 statementRequiredStackSize (VariableInitialisation _ expr) = expressionEvalStackSize expr
 statementRequiredStackSize (VariableAssignment _ expr) = expressionEvalStackSize expr
+statementRequiredStackSize (IfStatement predicate ifBody) = max (expressionEvalStackSize predicate) (maxStackSize ifBody)
+statementRequiredStackSize (IfElseStatement predicate ifBody elseBody) = max (expressionEvalStackSize predicate) (max (maxStackSize ifBody) (maxStackSize elseBody))
 
 expressionEvalStackSize :: Expression -> Int
 expressionEvalStackSize (ReferenceExpression _) = 1
