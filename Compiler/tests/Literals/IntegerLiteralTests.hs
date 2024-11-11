4b8d9c9043ef76b18278140f5a5937a11653003c
Mitchell Wright <mitchell.wright877@gmail.com>
2024-11-11 06:10:37 +0000

    Some moar tests & coverage

--- Compiler/tests/Literals/IntegerLiteralTests.hs
+++ Compiler/tests/Literals/IntegerLiteralTests.hs
@@ -2,20 +2,33 @@ module Literals.IntegerLiteralTests where
 
 import Test.HUnit
 import Text.Parsec
+import Data.Either
 import Literals
 
 integerLiteralTests :: Test
 integerLiteralTests = TestList [
-        TestLabel "Parse Positive Integer Literal" parsePositiveIntegerLiteralTest,
-        TestLabel "Parse Negative Integer Literal" parseNegativeIntegerLiteralTest
+        parsePositiveIntegerLiteralTest,
+        parseNegativeIntegerLiteralTest,
+        failParseCharacterIntegerLiteralTest,
+        failParseMixedCharacterIntegerLiteralTest
     ]
 
 parsePositiveIntegerLiteralTest :: Test
 parsePositiveIntegerLiteralTest = TestCase $ assertEqual "Should parse positive integer" 
-    (Right $ IntegerLiteral 12345) 
+    (Right . IntegerLiteral $ 12345) 
     (parse literal "" "12345")
 
 parseNegativeIntegerLiteralTest :: Test
 parseNegativeIntegerLiteralTest = TestCase $ assertEqual "Should parse positive integer" 
     (Right . IntegerLiteral $ -12345) 
     (parse literal "" "-12345")
+
+failParseCharacterIntegerLiteralTest :: Test
+failParseCharacterIntegerLiteralTest = TestCase $ assertEqual "Should fail parsing character string"
+    True
+    (isLeft $ parse literal "" "abc")
+
+failParseMixedCharacterIntegerLiteralTest :: Test
+failParseMixedCharacterIntegerLiteralTest = TestCase $ assertEqual "Should fail parsing a string with mix of digits & chars"
+    True
+    (isLeft $ parse literal "" "abc123")
